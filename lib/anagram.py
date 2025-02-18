6313c8f7d93807118d2cafced4a3dbcdf6fff15e
Valerie <britneyvalerie85@gmail.com>
2025-02-18 15:35:57 +0300

    changes added

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word: str):
+        self.word = word.lower()  
+
+    def match(self, words):
+        sorted_word = sorted(self.word)  
+        return [w for w in words if sorted(w.lower()) == sorted_word]  
